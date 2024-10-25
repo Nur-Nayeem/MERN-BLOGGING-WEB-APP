7ba1dae2517b03911d0a3748a579acb810e3e04b
Nur-Nayeem <nurnayem768@gmail.com>
2024-10-25 14:01:41 +0600

    remove extra line and create getFullDate funtion

--- blogging website - frontend/src/common/date.jsx
+++ blogging website - frontend/src/common/date.jsx
@@ -1,5 +1,4 @@
 let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Des"];
-let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
 
 export const getDay = (timestamp) => {
     let date = new Date(timestamp);
