a54c4728f36dda74b98720f17c96c81bbe304e44
Nur-Nayeem <nurnayem768@gmail.com>
2024-10-29 15:00:51 +0600

    make server for like blog and like_by_user

--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { response } from 'express';
 import mongoose from 'mongoose';
 import 'dotenv/config'
 import bcrypt from 'bcrypt';
@@ -26,6 +26,7 @@ cloudinary.config({
 //schema below
 import User from './Schema/User.js';
 import Blog from './Schema/Blog.js';
+import Notification from './Schema/Notification.js';
 
 
 const server = express();
@@ -470,6 +471,52 @@ server.post("/get-blog", (req, res) => {
         })
         .catch(err => {
             return res.status(500).json({ error: err.message });
+        })
+
+})
+
+server.post("/like-blog", verifyJWT, (req, res) => {
+    let user_id = req.user;
+    let { _id, isLikedByuser } = req.body;
+
+    let incrementVal = !isLikedByuser ? 1 : -1;
+
+    Blog.findOneAndUpdate({ _id }, { $inc: { "activity.total_likes": incrementVal } })
+        .then(blog => {
+            if (!isLikedByuser) {
+                let like = new Notification({
+                    type: "like",
+                    blog: _id,
+                    notification_for: blog.author,
+                    user: user_id
+                })
+                like.save().then(notification => {
+                    return res.status(200).json({ liked_by_user: true })
+                })
+            }
+            else {
+                Notification.findOneAndDelete({ user: user_id, blog: _id, type: "like" })
+                    .then(data => {
+                        return res.status(200).json({ liked_by_user: false })
+                    })
+                    .catch(err => {
+                        return res.status(500).json({ error: err.message })
+                    })
+            }
+        })
+
+})
+
+server.post("/isliked-by-user", verifyJWT, (req, res) => {
+    let user_id = req.user;
+    let { _id } = req.body;
+
+    Notification.exists({ user: user_id, type: "like", blog: _id })
+        .then(result => {
+            return res.status(200).json({ result })
+        })
+        .catch(err => {
+            return res.status(500).json({ error: err.message })
         })
 
 })
