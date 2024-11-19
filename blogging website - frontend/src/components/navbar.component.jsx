31de2235d0b81c2134fb3545675384458d784fe4
Nur-Nayeem <nurnayem768@gmail.com>
2024-11-19 19:21:17 +0600

    Notification status display

--- blogging website - frontend/src/components/navbar.component.jsx
+++ blogging website - frontend/src/components/navbar.component.jsx
@@ -1,16 +1,36 @@
 import { Link, Navigate, Outlet, useNavigate } from "react-router-dom";
 import logo4 from "../imgs/logo4.png";
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { UserContext } from "../App";
 import UserNavigationPanel from "./user-navigation.component";
+import axios from "axios";
 const Navbar = () => {
 
     const [searchBoxVisibility, setSearchBoxVisibility] = useState(false)
     const [userNavPanel, setUserNavPanel] = useState(false);
 
-    const { userAuth: { access_token, profile_img } } = useContext(UserContext);
+    const { userAuth, userAuth: { access_token, profile_img, new_notification_available }, setUserAuth } = useContext(UserContext);
 
     const navigate = useNavigate();
+
+    useEffect(() => {
+        if (access_token) {
+            axios.get(import.meta.env.VITE_SERVER_DOMAIN + "/new-notification", {
+                headers: {
+                    'Authorization': `Bearer ${access_token}`
+                }
+            })
+                .then(({ data }) => {
+                    setUserAuth({ ...userAuth, ...data })
+                })
+                .catch(err => {
+                    console.log(err);
+
+                })
+        }
+
+    }, [access_token])
+
     const handleUserNavPanel = () => {
         setUserNavPanel(currentVal => !currentVal);
     }
@@ -60,9 +80,15 @@ const Navbar = () => {
                 {
                     access_token ?
                         <>
-                            <Link to="/dashboard/notification">
+                            <Link to="/dashboard/notifications">
                                 <button className="w-12 h-12 rounded-full relative bg-grey hover:bg-black/10">
                                     <i className="fi fi-rr-bell text-2xl block mt-1"></i>
+                                    {
+                                        new_notification_available ?
+                                            <span className="bg-red w-3 h-3 rounded-full absolute z-10 top-2 right-2"></span>
+                                            :
+                                            ""
+                                    }
                                 </button>
                             </Link>
 
--- blogging website - frontend/src/components/sidenavbar.component.jsx
+++ blogging website - frontend/src/components/sidenavbar.component.jsx
@@ -3,7 +3,7 @@ import { Navigate, NavLink, Outlet } from "react-router-dom";
 import { UserContext } from "../App";
 
 const SideNav = () => {
-    let { userAuth: { access_token } } = useContext(UserContext)
+    let { userAuth: { access_token, new_notification_available } } = useContext(UserContext)
 
     let page = location.pathname.split('/')[2];
 
@@ -59,8 +59,18 @@ const SideNav = () => {
                                 <i className="fi fi-rr-document"></i>
                                 Blogs
                             </NavLink>
-                            <NavLink to="/dashboard/notification" onClick={(e) => setPageState(e.target.innerText)} className="sidebar-link">
-                                <i className="fi fi-rr-bell"></i>
+                            <NavLink to="/dashboard/notifications" onClick={(e) => setPageState(e.target.innerText)} className="sidebar-link">
+                                <div className="relative">
+                                    <i className="fi fi-rr-bell"></i>
+
+                                    {
+                                        new_notification_available ?
+                                            <span className="bg-red w-2 h-2 rounded-full absolute z-10 top-0 right-0"></span>
+                                            :
+                                            ""
+                                    }
+                                </div>
+
                                 Notification
                             </NavLink>
                             <NavLink to="/editor" onClick={(e) => setPageState(e.target.innerText)} className="sidebar-link">
